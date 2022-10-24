ed84ce95c60aded26ffe0e3a58b8dd866d81e35c
ESTUDIANTE <ESTUDIANTE@DESKTOP-N7TM1CP>
2022-10-24 13:52:56 -0500

    Conexion DB con Views

--- SGPI/Controllers/AdministradorController.cs
+++ SGPI/Controllers/AdministradorController.cs
@@ -8,29 +8,63 @@ namespace SGPI.Controllers
     {
         SGPIDBContext context = new SGPIDBContext();
 
+        [HttpPost]
+        public IActionResult Login(Usuario user)
+        {
+            var usuario = context.Usuarios
+                .Where(consulta => consulta.NumeroDocumento == user.NumeroDocumento
+                        && consulta.Password == user.Password).FirstOrDefault();
+
+            if (usuario != null)
+            {
+                if (usuario.IdRol == 1) 
+                {
+                    //Redirige a la vista principal de Administrador
+                    return View("MenuAdmBuscar");
+                }
+                    
+                else if (usuario.IdRol == 2) 
+                {
+                    //Redirige a la vista principal de Coordinador
+                    return Redirect("Coordinador/MenuCoordinadorBuscar");
+                }
+                    
+                else if (usuario.IdRol == 3) 
+                {
+                    //Redirige a la vista de Estudiante
+                    return Redirect("Estudiante/ActualizarEstudiante");
+                }
+                else {
+                }
+            } 
+
+            return View();
+        }
+
         public IActionResult Login()
         {
+
             //Genero genero = new Genero();
             //genero.Genero1 = "LGTBI";
 
             //context.Add(genero);
             //context.SaveChanges();
 
-            var gen = (from s in context.Generos 
-                       where s.IdGenero == 1 
-                       select s); 
-            //pruebsa
+            //var gen = (from s in context.Generos 
+            //           where s.IdGenero == 1 
+            //           select s); 
+            ////pruebsa
 
 
-            Genero nuevoGen = gen.SingleOrDefault();
+            //Genero nuevoGen = gen.SingleOrDefault();
 
-            nuevoGen.Genero1 = "Binario";
+            //nuevoGen.Genero1 = "Binario";
 
-            context.Update(nuevoGen);
-            context.SaveChanges();
+            //context.Update(nuevoGen);
+            //context.SaveChanges();
 
-            context.Remove(nuevoGen);
-            context.SaveChanges();
+            //context.Remove(nuevoGen);
+            //context.SaveChanges();
 
             return View();
         }
